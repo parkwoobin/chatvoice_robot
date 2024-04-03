739e5c4ba4c84e7872b3cfaf88c7e6c6944c5174
parkwoobin <oi3oi3oi@naver.com>
2024-04-04 03:32:43 +0900

    Update voice_txt_bot.py

--- voice_txt_bot.py
+++ voice_txt_bot.py
@@ -31,18 +31,19 @@ def main():
     st.set_page_config(page_title="음성 진우 프로그램")
 
     # 제목
-    st.header("🐼 음성 진우 프로그램")
+    st.header("🐼 AI 진우 프로그램")
     st.caption("A streamlit chatbot powered by OpenAI ParkWB & LeeSW")
 
     # 기본 설명
-    with st.expander("음성 진우 프로그램에 관하여", expanded=False):
+    with st.expander("AI 진우 프로그램에 관하여", expanded=False):
         
         st.write(
             """
-            - 음성 진우 프로그램은 Chat GPT API를 사용하였습니다.
+            - AI 진우 프로그램은 Chat GPT API를 사용하였습니다.
             - GPT 모델의 선택이 가능합니다.(gpt-3.5-turbo, gpt-4)
             - STT(Speech-To-Text)는 OpenAI의 Whisper AI를 활용하였습니다.
             - Chat GPT API key를 입력하지 않으면 작동이 되지 않으니 주의 바랍니다.
+            - 채팅 질문은 답변 아래에서, 음성 질문은 사이드바에서 가능합니다.
             - 강남대학교 박진우 학생을 모티브로 제작되었습니다.
             - 이 프로그램은 강남대학교 박우빈, 이승우 학생이 공동으로 제작하였습니다.
             """)
