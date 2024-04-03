5969ec65069abb47e62873ba734ab0616f35989e
parkwoobin <oi3oi3oi@naver.com>
2024-04-04 03:43:51 +0900

    Update voice_txt_bot.py

--- voice_txt_bot.py
+++ voice_txt_bot.py
@@ -40,7 +40,7 @@ def main():
         
         st.write(
             """
-            - AI 진우 프로젝트 Chat GPT API를 사용하였습니다.
+            - AI 진우 프로젝트 OpenAI API를 사용하였습니다.
             - GPT 모델의 선택이 가능합니다.(gpt-3.5-turbo, gpt-4)
             - STT(Speech-To-Text)는 OpenAI의 Whisper AI를 활용하였습니다.
             - Chat GPT API key를 입력하지 않으면 작동이 되지 않으니 주의 바랍니다.
